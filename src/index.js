f129eaf70105ea24e9cef610331650ddeac0ba97
Dan Zera <dczera@gmail.com>
2017-11-07 22:15:35 -0600

    render component try 2 - React.render deprecated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Tell transpiler to give this file access to React.
+import React from 'react'; // Goes and gets dependency/library named 'react' and assigns it to a variable named 'React'
+
 // Create a new component that produces some HTML.
 const App = function() {
 	// This appears to return HTML, but is actually JSX. Can't have HTML in J.S. files.
@@ -8,3 +11,9 @@ const App = function() {
 
 // Tell React to take this component's generated HTML and put it in/on the DOM.
 React.render(App);
+
+// running at this point yields:
+// "Warning: React.render is deprecated. Please use ReactDOM.render from require('react-dom') instead."
+// Original React library has diverged into two separate libraries.
+// React (core) library knows how to work with React components -- how to render them, nest them togeter...
+// ReactDOM library knows how to actually render the components to the DOM (insert them into the DOM)
